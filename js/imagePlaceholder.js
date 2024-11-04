79f9ab9dfa1d016093bf8ca5f849522b9d369cc9
sarah-pen <144973114+sarah-pen@users.noreply.github.com>
2024-11-04 15:56:37 -0500

    Update imagePlaceholder.js

--- js/imagePlaceholder.js
+++ js/imagePlaceholder.js
@@ -2,7 +2,7 @@ document.querySelectorAll("img").forEach( img => {
     console.log("runs")
     img.onerror = function() {
         this.onerror = null; // No infinite loop if default image is also missing
-        this.src = 'placeholder-image.jpg';
+        this.src = 'js/placeholder-image.jpg';
         this.alt = "Default image";
         };     
     }
